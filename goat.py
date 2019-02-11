3fc6fba7cc7bbdb395ef50d7700aaf2ac3065c86
Kevin Chen <30479452+kxvxnc@users.noreply.github.com>
2019-02-11 11:38:34 -0500

    Update goat.py

--- goat.py
+++ goat.py
@@ -18,23 +18,39 @@ async def goat(ctx, *args):
         r = s.post("https://2fwotdvm2o-dsn.algolia.net/1/indexes/product_variants_v2/query", params=x, verify=False, data=byte_payload, timeout=30)
     try:
         results = r.json()["hits"][0]
-        url = 'https://goat.com/sneakers/' + results['slug']
-        product = results['name']
-        sku = results['sku']
-        date = results['release_date'].split('T')[0]
-        lowest_used = '$' + str(results['lowest_price_cents'])[:-2]
+        priceurl = f"https://www.goat.com/api/v1/product_templates/{results['slug']}/show_v2"
+        recenturl = f"https://www.goat.com/api/v1/product_templates/{results['slug']}/recent_purchases?count=6"
+        bidurl = f"https://www.goat.com/api/v1/product_templates/{results['slug']}/highest_offers"
+        with requests.Session() as s2:
+            header = {
+                'accept': 'application/json',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+                'accept-language': 'en-us',
+                'accept-encoding': 'br,gzip,deflate'
+            }
+            r2 = s2.get(priceurl, headers=header)
+            prices = r2.json()
+            priceformat = ''
+            for size in prices['availableSizesNew']:
+                priceformat += f"Size {size[0]} ${str(int(size[1]))[:-2]}\n"
+            r3 = s2.get(recenturl, headers=header)
+            recent = r3.json()
+            recentformat = ''
+            for size in recent:
+                recentformat += f"${str(int(size['priceCents']))[:-2]} at {size['purchasedAt'].split('T')[0]} {size['purchasedAt'].split('T')[1].split('.')[0]} GMT\n"
+            r4 = s2.get(bidurl, headers=header)
+            bids = r4.json()
+            bidformat = ''
+            
     except:
         await client.say('An error occurred. Please check keywords again.')
-
-    embed = discord.Embed(title='GOAT Info Checker', color=0x13e79e)
-    embed.set_thumbnail(url=results['original_picture_url'])
+    embed = discord.Embed(title='GOAT App Checker', color=0x13e79e)
+    embed.set_thumbnail(url=prices['pictureUrl'])
     embed.set_footer(text='@kxvxnc#6989')
-    embed.add_field(name='Product Name:', value=f'[{product}]({url})', inline=True)
-    embed.add_field(name='SKU/PID:', value=sku, inline=True)
-    embed.add_field(name='Retail Price:', value='$' + str(results['special_display_price_cents'])[:-2], inline=True)
-    embed.add_field(name='Colorway:', value=results['details'], inline=True)
-    embed.add_field(name='Release Date:', value=date, inline=True)
-    embed.add_field(name='Lowest Used Price:', value=lowest_used, inline=True)
+    embed.add_field(name=prices['name'], value=f"https://goat.com/sneakers/{results['slug']}", inline=False)
+    embed.add_field(name='SKU | Release Date | Retail', value=f"{results['sku']} | {results['release_date'].split('T')[0]} | ${str(results['special_display_price_cents'])[:-2]}", inline=False)
+    embed.add_field(name='Prices:', value=priceformat, inline=True)
+    embed.add_field(name='Recent Sales:', value=recentformat, inline=True)
     await client.say(embed=embed)
 
 client.run(token)
